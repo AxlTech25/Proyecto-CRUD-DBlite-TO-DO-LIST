bc2b76001e690c46e58dda95aafdb9bf79d47208
AxlTech25 <70443740@continental.edu.pe>
2025-06-24 21:17:47 -0600

    Delete models directory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Base
-from .user import User
-from .task import Task, TaskCategory, TaskState, TaskPriority, TaskFrequency
-from .category import Category
-from .notification import Notification
